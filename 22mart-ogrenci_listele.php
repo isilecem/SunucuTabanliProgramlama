d9c0d0db97da24a8ef2b2ce4198f65158a3534a9
isilecem <eceemozel@gmail.com>
2018-03-22 11:23:08 +0300

    ogrenci_listele.php

---
+++ 22mart-ogrenci_listele.php
@@ -0,0 +1,27 @@
+<?php
+require("baglan.php");
+
+if(!$baglan){
+	die("Bağlantı başarısız".mysqli_connect_error());
+}
+else
+{
+	echo "Bağlantı başarılı<br>";
+}
+
+$sorgu="select * from ogrenci_bilgi";
+$a=mysqli_query($baglan,$sorgu);
+
+if(mysqli_num_rows($a)>0)
+	#ogrenci_bilgi tablomuzun içi boş değilse...
+{
+	while($row=mysqli_fetch_assoc($a))#bu komut VT'dan gelen bilgileri bir liste yapmak için
+	{
+		echo "Ogrenci no: ".$row["ogrenci_no"]." - Ad: ".$row["ad"]." - Soyad: ".$row["soyad"]." - Bolumu: ".$row["bolum"]."<br>";
+	}
+}
+else{
+	echo "Kayıt bulunamadı";
+}
+
+?>
