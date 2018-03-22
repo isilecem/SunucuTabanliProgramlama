aeda78d3159e0d308ed376991990b969211b9abf
isilecem <eceemozel@gmail.com>
2018-03-22 21:39:28 +0300

    ogrenci_listele

--- 22mart-ogrenci_listele.php
+++ 22mart-ogrenci_listele.php
@@ -1,6 +1,5 @@
 <?php
 require("baglan.php");
-
 if(!$baglan){
 	die("Bağlantı başarısız".mysqli_connect_error());
 }
@@ -8,14 +7,12 @@ else
 {
 	echo "Bağlantı başarılı<br>";
 }
-
 $sorgu="select * from ogrenci_bilgi";
 $a=mysqli_query($baglan,$sorgu);
-
 if(mysqli_num_rows($a)>0)
-	#ogrenci_bilgi tablomuzun içi boş değilse...
+	# "ogrenci_bilgi" tablomuzun içi boş değilse... diye bu if'li kod
 {
-	while($row=mysqli_fetch_assoc($a))#bu komut VT'dan gelen bilgileri bir liste yapmak için
+	while($row=mysqli_fetch_assoc($a))# bu komut veritabanından gelen bilgileri bir liste yapmak için
 	{
 		echo "Ogrenci no: ".$row["ogrenci_no"]." - Ad: ".$row["ad"]." - Soyad: ".$row["soyad"]." - Bolumu: ".$row["bolum"]."<br>";
 	}
@@ -23,7 +20,6 @@ if(mysqli_num_rows($a)>0)
 else{
 	echo "Kayıt bulunamadı";
 }
-
 mysqli_close($baglan);
-
+# mysql'i kapatmazsak veritabanına yük biner, bu nedenle en son "close" ediyoruz
 ?>
