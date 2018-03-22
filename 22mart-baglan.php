9fa777bb7b421e32e4f56e0a3daedd5abc90f164
isilecem <eceemozel@gmail.com>
2018-03-22 21:40:40 +0300

    baglan

--- 22mart-baglan.php
+++ 22mart-baglan.php
@@ -1,6 +1,6 @@
 <?php
 $baglan=mysqli_connect("localhost","root","","ogrenci");
-#server'in ismi,kullanıcı ismi,şifre,veritabanı ismi
+# server'in ismi, kullanıcı adı, şifre, veritabanının ismi
 mysqli_query($baglan,"SET NAMES UTF8");
-
+# baglan.php'yi neden oluşturuyoruz? çünkü büyük projelerde zorluk olmasın diye, sürekli tekrarı önlemek amacıyla
 ?>
