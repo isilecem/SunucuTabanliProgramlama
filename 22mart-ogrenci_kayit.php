219fb4e1dfc8edefd6645f0604762ffd94fb0754
isilecem <eceemozel@gmail.com>
2018-03-22 21:39:55 +0300

    ogrenci_kayit

--- 22mart-ogrenci_kayit.php
+++ 22mart-ogrenci_kayit.php
@@ -1,25 +1,25 @@
 <?php
 require("baglan.php");
-#baglan.php'yi çağırmak için require komutu / include veya require_once da kullanılabilir
+# baglan.php'yi çağırmak için require komutu kullanılır / "include" veya "require_once" da kullanılabilir
+# "include" sunucuya yük bindirir, tüm kodları çalıştırır ama baglan.php'de bir sıkıntı olup kodlar silinse veya bağlantı yapılmasa dahi sistem çalışır
 if(!$baglan){
 	die("Bağlantı başarısız".mysqli_connect_error());
 }
-#die: break komutu ile aynı anlama geliyor kod başarısız olursa aşağıdaki kodlara geçmiyor
-#hatanın nerede olduğunu göstermek için mysqli_connect_error
+# "die", break komutu ile aynı anlama geliyor, kod başarısız olursa aşağıdaki kodlara geçmiyor
+# hatanın nerede olduğunu göstermek için "mysqli_connect_error"
 else
 {
-	echo "Bağlantı başarılı";
+	echo "Bağlantı başarılı<br>";
 }
 $ogrno=$_POST["ogr_no"];
 $ad=$_POST["ad"];
 $soyad=$_POST["soyad"];
 $bolum=$_POST["bolum"];
-
 if($ogrno&$ad&$soyad&$bolum)
 {
 	$sorgu="insert into ogrenci_bilgi(ogrenci_no, ad, soyad, bolum) values('$ogrno','$ad','$soyad','$bolum')";
 	
-	if(mysqli_query($baglan,$sorgu)===True)#=== hem türü aynı olacak hem de değer aynı olacak // == sadece değerler aynı olacak
+	if(mysqli_query($baglan,$sorgu)===True)# "===" hem türü aynı olacak hem de değer aynı olacak // "==" sadece değerler aynı olacak
 	{
 		echo "Kayıtlar başarıyla eklendi";
 	}	
@@ -30,5 +30,4 @@ if($ogrno&$ad&$soyad&$bolum)
 else{
 	echo "Lütfen tüm alanları doldurun";
 }
-
 ?>
